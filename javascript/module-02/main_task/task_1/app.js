48f18ced424b99ab767167704ceda23875ffb636
Denys <meerzondenis91@gmail.com>
2019-04-05 06:39:12 +0300

    the main_task of module_2

--- javascript/module-02/main_task/task_1/app.js
+++ javascript/module-02/main_task/task_1/app.js
@@ -1,21 +1,22 @@
 'use strict';
-// let input;
-// const numbers = [];
-// let total = 0;
+let input;
+const numbers = [];
+let total = 0;
 
-// do {
-//   input = prompt('Введите число');
-//   if (Number.isNaN(input) === true) {
-//     alert('Было введено не число, попробуйте еще раз');
-//   } else {
-//     numbers.push(Number(input));
-//   }
-// } while (input !== null);
-// console.log(numbers);
-// if (numbers.length > 0) {
-//   for (let i = 0; i <= numbers.length; i += 0) {
-//     console.log(numbers[i]);
-//   }
-// }
-// for (i=0; numbers[total])
-// console.log(numbers);
+do {
+  input = prompt('Введите число');
+  input = Number(input);
+  const inputIsNaN = Number.isNaN(input);
+  if (inputIsNaN === false) {
+    numbers.push(input);
+  } else {
+    message = alert('Было введено не число, попробуйте еще раз');
+  }
+} while (input !== 0);
+numbers.pop();
+if (numbers.length > 0) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    total += numbers[i];
+  }
+  console.log(`Общая сумма чисел равна: ${total}`);
+}
