961c5c9a5528d2721e84c2126bb44342f66649a1
Denys <meerzondenis91@gmail.com>
2019-04-08 05:17:18 +0300

    fixed main task of module_02

--- javascript/module-02/main_task/task_2/app.js
+++ javascript/module-02/main_task/task_2/app.js
@@ -1,20 +1,17 @@
 'use strict';
 const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let attemptsLeft = 3;
-let message;
 do {
   const userPassword = prompt('Введите свой пароль');
   if (userPassword === null) {
-    message = alert('Вы ничего не ввели!');
+    alert('Вы ничего не ввели!');
   } else if (passwords.includes(userPassword) === true) {
-    message = alert('Добро пожаловать!');
+    alert('Добро пожаловать!');
     break;
   } else if (passwords.includes(userPassword) === false && attemptsLeft > 1) {
-    message = alert(
-      `Неверный пароль, у вас осталось ${attemptsLeft - 1} попыток`,
-    );
+    alert(`Неверный пароль, у вас осталось ${attemptsLeft - 1} попыток`);
   } else if (passwords.includes(userPassword) === false && attemptsLeft === 1) {
-    message = alert('У вас закончились попытки, аккаунт заблокирован!');
+    alert('У вас закончились попытки, аккаунт заблокирован!');
   }
   if (userPassword !== null) {
     attemptsLeft -= 1;
