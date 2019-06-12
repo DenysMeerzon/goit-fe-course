c80690ff97095f01aa659e317e75b1f070c958eb
Denys <meerzondenis91@gmail.com>
2019-06-12 18:06:01 +0300

    the additional_task of module 8

--- javascript/module-08/additional_task/js/sixthTask.js
+++ javascript/module-08/additional_task/js/sixthTask.js
@@ -9,29 +9,7 @@
   Используйте createElement для создания узлов.
   Добавьте классы и атрибуты.
 */
-{
-  /* <div class="movie">
-      <img
-        class="movie__image"
-        src="http://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg"
-        alt="movie image"
-      />
 
-      <div class="movie__body">
-        <h2 class="movie__title">The Godfather</h2>
-        <p class="movie__description">
-          Spanning the years 1945 to 1955, a chronicle of the fictional
-          Italian-American Corleone crime family. When organized crime family
-          patriarch, Vito Corleone barely survives an attempt on his life, his
-          youngest son, Michael steps in to take care of the would-be killers,
-          launching a campaign of bloody revenge.
-        </p>
-
-        <p class="movie__date">Released: 1972-03-14</p>
-        <p class="movie__rating">Rating: 8.6</p>
-      </div>
-    </div> */
-}
 const movieItem = [
   {
     src: 'http://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg',
@@ -43,6 +21,13 @@ const movieItem = [
     movieRating: '8.6',
   },
 ];
+
+const newMovieItems = document.querySelector('body');
+
+function getMovieCard(movieItems) {
+  movieItems.map(item => createMovieCard(item));
+}
+
 function createMovieCard(newItem) {
   const movie = document.createElement('div');
   movie.classList.add('movie');
@@ -52,7 +37,29 @@ function createMovieCard(newItem) {
   theImg.setAttribute('src', newItem.src);
   theImg.setAttribute('alt', newItem.alt);
 
-  movie.appendChild(theImg);
-  return movie;
+  const movieBody = document.createElement('div');
+  movieBody.classList.add('movie__body');
+
+  const movieTitle = document.createElement('h2');
+  movieTitle.classList.add('movie__title');
+  movieTitle.textContent = newItem.movieTitle;
+
+  const movieDescription = document.createElement('p');
+  movieDescription.classList.add('movie__description');
+  movieDescription.textContent = newItem.movieDescription;
+
+  const movieDate = document.createElement('p');
+  movieDate.classList.add('movie__date');
+  movieDate.textContent = `Released: ${newItem.movieDate}`;
+
+  const movieRating = document.createElement('p');
+  movieRating.classList.add('movie__rating');
+  movieRating.textContent = `Rating: ${newItem.movieRating}`;
+
+  movieBody.append(movieTitle, movieDescription, movieDate, movieRating);
+
+  movie.append(theImg, movieBody);
+
+  newMovieItems.insertAdjacentElement('afterbegin', movie);
 }
-createMovieCard(movieItem);
+getMovieCard(movieItem);
